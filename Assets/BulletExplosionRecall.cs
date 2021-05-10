bef1f99ee72992e8f4b733be8b61813fb9099674
Kota Z <pineapplegamedesign@gmail.com>
2021-05-10 02:40:16 -0400

    First Steps into Particles;Fixed Arrows
    Arrrows particles added, and explosion for effect. Need some help with lights, but it's nice so far.

---
+++ Assets/BulletExplosionRecall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletExplosionRecall : MonoBehaviour
+{
+    public GameObject BulletOwner;
+    public void OnParticleSystemStopped()
+    {
+        Debug.Log("BaseBull: Exploded :pppp");
+        BulletOwner.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Combat/Combat Class/BaseBullet.cs
+++ Assets/Scripts/Combat/Combat Class/BaseBullet.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class BaseBullet : MonoBehaviour, IPooledObj
 {
+    public GameObject BulletFX;
+    public GameObject DeathFX;
     private LayerMask enemies;
     private Hit dmg;
     private GameObject owner;
     public void OnObjectSpawn()
     {
 
+        BulletFX.SetActive(true);
+
     }
     public void SetBullet(LayerMask inmies, Hit inDmg, GameObject owna)
     {
+
         enemies = inmies;
         dmg = inDmg;
         owner = owna;
+
     }
     private void OnTriggerEnter(Collider other) {
 
@@ -32,13 +38,28 @@ public class BaseBullet : MonoBehaviour, IPooledObj
                     StatsManager hitStats = other.GetComponent<StatsManager>();
                     if(hitStats != null) hitStats.TakeAttack(dmg);
                     else Debug.Log("BaseBullet: Tried to hurt nobody :(");
+                    
+                    if (DeathFX!=null) 
+                    {
+                    Debug.Log("BaseBull: Explode!");
+                    BulletFX.SetActive(false);
+                    DeathFX.SetActive(true);
+                    }
+                    else
+                    {
+                    this.gameObject.SetActive(false);
+                    }
+
                 }
-                this.gameObject.SetActive(false);
+
+                //set up wall layer
+                
+                
             }
         }
-
-        
         
     }
+
+
 
 }

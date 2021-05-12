19dab166dd2ef3383bf3c9ede6ddd8013fbc8085
Kota Z <pineapplegamedesign@gmail.com>
2021-05-12 02:30:05 -0400

    Ok, actual stopping point for the night 5/11
    Work on Generation, and Game Management next.

---
+++ Assets/Scripts/Scene Management/ItemGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    //Generate the items for the world.
+    
+}
---
+++ Assets/Scripts/Scene Management/TotalGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalGenerator : MonoBehaviour
+{
+    
+    //Track all three generators, and seed./
+    //Send seed to level and item generator.
+    //jk.
+
+}
